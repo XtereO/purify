60ab94158e669723606d49d4a252d263c9746efa
unknown <kolahola149@gmail.com>
2023-05-14 12:30:23 +0300

    fix icons

--- src/icons/ScoreHighIcon.tsx
+++ src/icons/ScoreHighIcon.tsx
@@ -9,7 +9,7 @@ export const ScroeHighIcon = React.memo(() => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <g clip-path="url(#clip0_27:816)">
+      <g clipPath="url(#clip0_27:816)">
         <path
           d="M1.33447 16.0456L8.50998 3.23219C10.0373 0.504738 13.9627 0.504738 15.49 3.23219L22.6655 16.0456C24.1587 18.7119 22.2314 22 19.1755 22H4.8245C1.76856 22 -0.158668 18.7119 1.33447 16.0456Z"
           fill="#FF4B4B"
--- src/icons/ScoreLowIcon.tsx
+++ src/icons/ScoreLowIcon.tsx
@@ -9,7 +9,7 @@ export const ScoreLowIcon = React.memo(() => {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <rect width="24" height="24" rx="12" fill="#2ED954" fill-opacity="0.2" />
+      <rect width="24" height="24" rx="12" fill="#2ED954" fillOpacity="0.2" />
       <path
         d="M7 12.9064L9.8125 15.6154L17 8.69232"
         stroke="#2ED954"
